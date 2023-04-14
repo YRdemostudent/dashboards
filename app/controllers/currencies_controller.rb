737fbaecb6e9c1a5750442eabce35a27cb55423a
Yanet Roman <yanet_0688@yahoo.com>
2023-04-14 01:12:42 +0000

    change one to 1

--- app/controllers/currencies_controller.rb
+++ app/controllers/currencies_controller.rb
@@ -20,4 +20,17 @@ def second_currency
   render(:template=> "currency_templates/step_two.html.erb")
 end 
 
+def convert_currency
+  #{"from_symbol"=> "ALL", "to_exchange"=> "AED"
+  @from_symbol =params.fetch("from_currency")
+  @to_symbol = params.fetch("to_convert")
+  
+  # TODO: replace USD and EUR with from_symbol and to_symbol
+  api_url ="https://api.exchangerate.host/convert?from=#{@from_symbol}&to=#{@to_symbol}"
+  @raw_data = open(api_url).read
+  @parsed_data = JSON.parse(@raw_data)
+  
+  @rate = @parsed_data.fetch("result")
+  render(:template=>"currency_templates/step_three.html.erb")
+end 
 end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get("/forex", {:controller => "currencies", :action => "first_currency"})
   get("/forex/:from_currency", {:controller=> "currencies", :action => "second_currency"})
-  
+  get("/forex/:from_currency/:to_convert", {:controller=> "currencies", :action => "convert_currency"})
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
